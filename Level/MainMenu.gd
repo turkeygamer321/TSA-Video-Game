0b24c216a477674e1e7ee1937850144bb45503b9
Adan Dubas <dubasTVR@richlandrams.net>
2024-01-10 23:05:51 -0500

    added main menu

--- Level/MainMenu.gd
+++ Level/MainMenu.gd
@@ -1,5 +1,16 @@
 extends Control
 
+func _ready():
+	$VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Level/L1.tscn")
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	pass # Replace with function body.
+	get_tree().quit()
