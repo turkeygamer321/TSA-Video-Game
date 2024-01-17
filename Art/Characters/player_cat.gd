ebb82c2b4ab8d4ac1c663de13b3ada90fd688b3e
Adan Dubas <dubasTVR@richlandrams.net>
2024-01-16 22:57:28 -0500

    Added crates and almost done adding search crate / interactable items functionality. encountered error and too tired to fix. dont know what to do this is the youtube video I used https://www.youtube.com/watch?v=ajCraxGAeYU

--- Art/Characters/player_cat.gd
+++ Art/Characters/player_cat.gd
@@ -9,11 +9,9 @@ func _physics_process(_delta):
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
-	
 	#Update velocity
-	velocity = input_direction * moves_speed
-	velocity.normalized()
+	velocity = input_direction.normalized() * moves_speed
+	print(velocity)
 	
 	#Move and Slide function uses velocity of character body to move character on map
 	move_and_slide()
-	
---
+++ Art/Items/Interactable Items/InteractableCrate.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+
+@onready var interaction_area = $InteractionArea
+@onready var sprite = $Sprite2D
+
+
+func _ready():
+	interaction_area.interact = Callable(self, "_search_crate")
+
+
+func _search_crate():
+	sprite.frame = 1 if sprite.frame == 0 else 0
